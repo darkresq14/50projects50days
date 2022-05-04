9c7b5211206496277fbbfa53c9c9d2a02c89b968
dArKrEsQ <razvan.bielz@gmail.com>
2022-05-04 17:25:59 +0300

    expanding cards

---
+++ 01-expanding-cards/script.js
@@ -0,0 +1,14 @@
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# 50 Projects In 50 Days - HTML, CSS & JavaScript
+
+> Author: Razvan Bielz
+> Date: 04.05.2022
+
+### Project Starter Boilerplate _PROJECT_STARTER_
+- includes fontawesome
